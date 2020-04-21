c2d1dab441e465d7a16acb2d0688b16f31de13ee
Ognjen Blazic <ognjen.blazic.dev@gmail.com>
2020-04-21 17:16:50 +0200

    Memory management - LOH allocation/deallocation with GC collect

---
+++ Playground/Playground/MemoryManagement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Playground
+{
+    public class MemoryManagement
+    {
+        public static void AllocateAndCleanupLargeMemory()
+        {
+            var heap = new byte[2][];
+
+            // allocate two 2GB array
+            // observe memory consumption
+            for (var i = 0; i < heap.GetLength(0); i++)
+            {
+                heap[i] = new byte[2147483591]; // a bit less than int.MaxValue
+            }
+
+            // dereference heap
+            for (var i = 0; i < heap.GetLength(0); i++)
+            {
+                heap[i] = null;
+            }
+
+            heap = null;
+           
+            var totalMemory = GC.GetTotalMemory(false); // observe memory consumption
+        }
+    }
+}
---
+++ Playground/TestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Playground;
+using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryManagement.AllocateAndCleanupLargeMemory();
+            var totalMemory = GC.GetTotalMemory(true); // all returned to normal
+        }
+    }
+}
