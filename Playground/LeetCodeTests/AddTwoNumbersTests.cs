b5de70460cd67a6b89d9c33829f55a661d18096c
Ognjen Blazic <ognjen.blazic.dev@gmail.com>
2021-02-15 10:28:04 +0100

    Tests refactored

--- Playground/LeetCodeTests/AddTwoNumbersTests.cs
+++ Playground/LeetCodeTests/AddTwoNumbersTests.cs
@@ -1,5 +1,8 @@
 using LeetCode;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCodeTests
 {
@@ -10,76 +13,76 @@ namespace LeetCodeTests
         [Test]
         public void Example1Test()
         {
-            var l1 = new ListNode(2);
-            l1.next = new ListNode(4);
-            l1.next.next = new ListNode(3);
-
-            var l2 = new ListNode(5);
-            l2.next = new ListNode(6);
-            l2.next.next = new ListNode(4);
-
-            var actual = subject.Result(l1, l2);
-
-            var expected = new ListNode(7);
-            expected.next = new ListNode(0);
-            expected.next.next = new ListNode(8);
-
-            Assert.AreEqual(expected.val, actual.val);
-            Assert.AreEqual(expected.next.val, actual.next.val);
-            Assert.AreEqual(expected.next.next.val, actual.next.next.val);
-            Assert.AreEqual(expected.next.next.next, actual.next.next.next);
+            Test(342, 465);
         }
 
         [Test]
         public void Example2Test()
         {
-            var l1 = new ListNode(0);
-            var l2 = new ListNode(0);
-
-            var actual = subject.Result(l1, l2);
-
-            var expected = new ListNode(0);
-
-            Assert.AreEqual(expected.val, actual.val);
-            Assert.AreEqual(expected.next, actual.next);
+            Test(0, 0);
         }
 
         [Test]
         public void Example3Test()
         {
-            var l1 = new ListNode(9);
-            l1.next = new ListNode(9);
-            l1.next.next = new ListNode(9);
-            l1.next.next.next = new ListNode(9);
-            l1.next.next.next.next = new ListNode(9);
-            l1.next.next.next.next.next = new ListNode(9);
-            l1.next.next.next.next.next.next = new ListNode(9);
+            Test(9999999, 9999);
+        }
 
-            var l2 = new ListNode(9);
-            l2.next = new ListNode(9);
-            l2.next.next = new ListNode(9);
-            l2.next.next.next = new ListNode(9);
+        [Test]
+        public void RandomTest()
+        {
+            Random r = new Random();
+            int rInt1 = r.Next(0, short.MaxValue);
+            int rInt2 = r.Next(0, short.MaxValue);
+
+            Test(rInt1, rInt2);
+        }
+
+        private void Test(int first, int second)
+        {
+            var l1 = IntegerToLinkedListNode(first);
+            var l2 = IntegerToLinkedListNode(second);
 
             var actual = subject.Result(l1, l2);
 
-            var expected = new ListNode(8);
-            expected.next = new ListNode(9);
-            expected.next.next = new ListNode(9);
-            expected.next.next.next = new ListNode(9);
-            expected.next.next.next.next = new ListNode(0);
-            expected.next.next.next.next.next = new ListNode(0);
-            expected.next.next.next.next.next.next = new ListNode(0);
-            expected.next.next.next.next.next.next.next = new ListNode(1);
+            Assert.AreEqual(first + second, LinkedListNodeToInteger(actual));
+        }
 
-            Assert.AreEqual(8, actual.val);
-            Assert.AreEqual(9, actual.next.val);
-            Assert.AreEqual(9, actual.next.next.val);
-            Assert.AreEqual(9, actual.next.next.next.val);
-            Assert.AreEqual(0, actual.next.next.next.next.val);
-            Assert.AreEqual(0, actual.next.next.next.next.next.val);
-            Assert.AreEqual(0, actual.next.next.next.next.next.next.val);
-            Assert.AreEqual(1, actual.next.next.next.next.next.next.next.val);
-            Assert.AreEqual(null, actual.next.next.next.next.next.next.next.next);
+        private ListNode IntegerToLinkedListNode(int number)
+        {
+            ListNode head = null;
+            ListNode tail = null;
+
+            var reverseStringNumber = number.ToString().Reverse().ToArray();
+
+            for(var i = 0; i < reverseStringNumber.Length; i++)
+            {
+                var numberNode = new ListNode((int)char.GetNumericValue(reverseStringNumber[i]));
+                if (i == 0)
+                {
+                    head = numberNode;
+                    tail = head;
+                    continue;
+                }
+
+                tail.next = numberNode;
+                tail = tail.next;
+            }
+
+            return head;
+        }
+
+        private int LinkedListNodeToInteger(ListNode numberNode)
+        {
+            var numbers = new List<int>();
+
+            while(numberNode != null)
+            {
+                numbers.Add(numberNode.val);
+                numberNode = numberNode.next;
+            }
+
+            return int.Parse(string.Join("", numbers.ToArray().Reverse()));
         }
     }
 }
