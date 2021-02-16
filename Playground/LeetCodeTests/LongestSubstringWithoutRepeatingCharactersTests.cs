872192ad84f73b3cf794acc8f162dd2e38e3896c
Ognjen Blazic <ognjen.blazic.dev@gmail.com>
2021-02-16 17:53:12 +0100

    LongestSubstringWithoutRepeatingCharacters - setup

---
+++ Playground/LeetCodeTests/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,39 @@
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    public class LongestSubstringWithoutRepeatingCharactersTests
+    {
+        private LongestSubstringWithoutRepeatingCharacters subject = new LongestSubstringWithoutRepeatingCharacters();
+
+        [Test]
+        public void Example1Test()
+        {
+            var actual = subject.LengthOfLongestSubstring("abcabcbb");
+            Assert.AreEqual(3, actual);
+        }
+
+        [Test]
+        public void Example2Test()
+        {
+            var actual = subject.LengthOfLongestSubstring("bbbbb");
+            Assert.AreEqual(1, actual);
+        }
+
+        [Test]
+        public void Example3Test()
+        {
+            var actual = subject.LengthOfLongestSubstring("pwwkew");
+            Assert.AreEqual(3, actual);
+        }
+
+        [Test]
+        public void Example4Test()
+        {
+            var actual = subject.LengthOfLongestSubstring("");
+            Assert.AreEqual(0, actual);
+        }
+    }
+
+}
